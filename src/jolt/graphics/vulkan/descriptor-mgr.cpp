5bfa126b997cba32fc30d8ab6679bf09073a6d48
Alfredo Mungo <alfredo.mungo@protonmail.ch>
2020-10-21 23:41:43 +0100

    Added descriptor manager

---
+++ src/jolt/graphics/vulkan/descriptor-mgr.cpp
@@ -0,0 +1,84 @@
+#include <jolt/graphics/vulkan.hpp>
+
+namespace jolt {
+    namespace graphics {
+        namespace vulkan {
+            DescriptorManager::DescriptorManager(
+              Renderer &renderer, uint32_t const max_descriptor_sets, pool_size_vector const &pool_sizes) :
+              m_renderer{renderer} {
+                create_descriptor_pool(max_descriptor_sets, pool_sizes);
+            }
+
+            DescriptorManager::~DescriptorManager() { destroy_descriptor_pool(); }
+
+            void DescriptorManager::create_descriptor_pool(
+              uint32_t const max_descriptor_sets, pool_size_vector const &pool_sizes) {
+                VkDescriptorPoolCreateInfo pcinfo{
+                  VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO,     // sType
+                  nullptr,                                           // pNext
+                  VK_DESCRIPTOR_POOL_CREATE_FREE_DESCRIPTOR_SET_BIT, // flags
+                  max_descriptor_sets,                               // maxSets
+                  static_cast<uint32_t>(pool_sizes.get_length()),    // poolSizeCount
+                  &pool_sizes[0]                                     // pPoolSizes
+                };
+
+                VkResult result = vkCreateDescriptorPool(
+                  get_renderer().get_device(), &pcinfo, get_vulkan_allocator(), &m_descriptor_pool);
+                jltassert2(result == VK_SUCCESS, "Unable to create graphics pipeline descriptor pool");
+            }
+
+            void DescriptorManager::destroy_descriptor_pool() {
+                vkDestroyDescriptorPool(
+                  get_renderer().get_device(), get_descriptor_pool(), get_vulkan_allocator());
+            }
+
+            VkDescriptorSetLayout DescriptorManager::create_descriptor_set_layout(
+              descriptor_set_layout_binding_vector const &bindings) {
+                VkDescriptorSetLayout layout;
+
+                VkDescriptorSetLayoutCreateInfo cinfo{
+                  VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO, // sType
+                  nullptr,                                             // pNext
+                  0,                                                   // flags
+                  static_cast<uint32_t>(bindings.get_length()),        // bindingCount
+                  &bindings[0],                                        // pBindings
+                };
+
+                VkResult result = vkCreateDescriptorSetLayout(
+                  get_renderer().get_device(), &cinfo, get_vulkan_allocator(), &layout);
+                jltassert2(result == VK_SUCCESS, "Unable to create descriptor set layout");
+
+                return layout;
+            }
+
+            void DescriptorManager::destroy_descriptor_set_layout(VkDescriptorSetLayout layout) {
+                vkDestroyDescriptorSetLayout(get_renderer().get_device(), layout, get_vulkan_allocator());
+            }
+
+            void DescriptorManager::destroy_pipeline_layout(VkPipelineLayout layout) {
+                vkDestroyPipelineLayout(get_renderer().get_device(), layout, get_vulkan_allocator());
+            }
+
+            VkPipelineLayout DescriptorManager::create_pipeline_layout(
+              desc_set_layout_vector const &layouts, push_const_range_vector const &pc_ranges) {
+                VkPipelineLayoutCreateInfo cinfo{
+                  VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO, // sType
+                  nullptr,                                       // pNext
+                  0,                                             // flags
+                  static_cast<uint32_t>(layouts.get_length()),   // setLayoutCount
+                  &layouts[0],                                   // pSetLayouts
+                  static_cast<uint32_t>(pc_ranges.get_length()), // pushConstanteRangeCount
+                  &pc_ranges[0]                                  // pPushConstantRanges
+                };
+
+                VkPipelineLayout pipeline_layout;
+
+                VkResult result = vkCreatePipelineLayout(
+                  get_renderer().get_device(), &cinfo, get_vulkan_allocator(), &pipeline_layout);
+                jltassert2(result == VK_SUCCESS, "Unable to create pipeline layout");
+
+                return pipeline_layout;
+            }
+        } // namespace vulkan
+    }     // namespace graphics
+} // namespace jolt
---
+++ src/jolt/graphics/vulkan/descriptor-mgr.hpp
@@ -0,0 +1,80 @@
+#ifndef JLT_GRAPHICS_VULKAN_DESCRIPTOR_MGR_HPP
+#define JLT_GRAPHICS_VULKAN_DESCRIPTOR_MGR_HPP
+
+#include <jolt/api.hpp>
+#include <jolt/collections/vector.hpp>
+#include "defs.hpp"
+
+namespace jolt {
+    namespace graphics {
+        namespace vulkan {
+            class Renderer;
+
+            class JLTAPI DescriptorManager {
+              public:
+                using desc_set_layout_vector = collections::Vector<VkDescriptorSetLayout>;
+                using push_const_range_vector = collections::Vector<VkPushConstantRange>;
+                using descriptor_set_layout_binding_vector =
+                  collections::Vector<VkDescriptorSetLayoutBinding>;
+                using pool_size_vector = collections::Vector<VkDescriptorPoolSize>;
+
+              private:
+                Renderer &m_renderer;
+                VkDescriptorPool m_descriptor_pool;
+
+                void create_descriptor_pool(
+                  uint32_t const max_descriptor_sets, pool_size_vector const &pool_sizes);
+                void destroy_descriptor_pool();
+
+              public:
+                /**
+                 * Create a new descriptor manager.
+                 *
+                 * @param renderer The renderer.
+                 * @param max_descriptor_sets Maximum number of descriptor sets allocatable from the
+                 * descriptor pool.
+                 * @param pool_sizes Collection of pool size objects to initialize the descriptor pool.
+                 */
+                DescriptorManager(
+                  Renderer &renderer, uint32_t const max_descriptor_sets, pool_size_vector const &pool_sizes);
+                ~DescriptorManager();
+
+                VkDescriptorPool get_descriptor_pool() const { return m_descriptor_pool; }
+                Renderer const &get_renderer() const { return m_renderer; }
+
+                /**
+                 * Create a new descriptor set layout.
+                 *
+                 * @param bindings The collection of bindings for the layout.
+                 */
+                VkDescriptorSetLayout
+                create_descriptor_set_layout(descriptor_set_layout_binding_vector const &bindings);
+
+                /**
+                 * Destroy a descriptor set layout.
+                 *
+                 * @param layout The descriptor set layout.
+                 */
+                void destroy_descriptor_set_layout(VkDescriptorSetLayout layout);
+
+                /**
+                 * Create a new pipeline layout.
+                 *
+                 * @param layouts The descriptor set layouts.
+                 * @param pc_ranges The push constant ranges.
+                 */
+                VkPipelineLayout create_pipeline_layout(
+                  desc_set_layout_vector const &layouts, push_const_range_vector const &pc_ranges);
+
+                /**
+                 * Destroy a pipeline layout.
+                 *
+                 * @param layout The pipeline layout.
+                 */
+                void destroy_pipeline_layout(VkPipelineLayout layout);
+            };
+        } // namespace vulkan
+    }     // namespace graphics
+} // namespace jolt
+
+#endif /* JLT_GRAPHICS_VULKAN_DESCRIPTOR_MGR_HPP */
