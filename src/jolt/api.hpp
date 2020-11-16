be2679c084de8498ec730cfe24f51b05fdfe7373
Alfredo Mungo <alfredo.mungo@protonmail.ch>
2020-11-16 23:39:22 +0000

    Added support for branch likelyhood attributes

--- src/jolt/api.hpp
+++ src/jolt/api.hpp
@@ -12,4 +12,15 @@
 #define JLT_MAYBE_UNUSED [[maybe_unused]]
 #define JLT_NODISCARD [[nodiscard]]
 
+// Required to prevent clangd from treating unknown attributes as errors.
+#if __clang_major__ >= 12
+    #warning Remove if/else block
+
+    #define JLT_LIKELY [[likely]]
+    #define JLT_UNLIKELY [[unlikely]]
+#else // __clang_major__ >= 12
+    #define JLT_LIKELY
+    #define JLT_UNLIKELY
+#endif // __clang_major__ >= 12
+
 #endif /* JLT_API_HPP */
